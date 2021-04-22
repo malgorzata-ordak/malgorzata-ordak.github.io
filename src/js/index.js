aafc3d7862471cb5f3b2ad02927ffdbef8460422
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2021-04-22 12:48:06 +0200

    added list of repo (Fech API)

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,47 @@
 import '../scss/main.scss';
 
 console.log('HELLO 🚀')
+
+fetch ("https://api.github.com/users/malgorzata-ordak/repos")
+.then (resp => resp.json ())
+.then (resp => {
+   const container =document.querySelector('.project-grid--js')
+   for (let repo of resp) {
+       console.log(repo)
+       const {description, homepage, html_url, name}= repo;
+       const template =`<article class="project">
+       <div class="project__bar">
+         <span class="project__element"></span>
+         <span class="project__element"></span>
+         <span class="project__element"></span>
+       </div>
+       <div class="project__box">
+         <img src="img/Github-Icon.svg" class="project__box-icon" alt="" />
+         <h3 class="project__grid project__h3">
+           <span class="project__span-one">project: </span>
+           <span class="project__span-two">${name}</span>
+         </h3>
+         <p class="project__grid">
+           <span class="project__span-one project__span-one--special">description:</span>
+           <span class="project__span-three project__span-three--special">${description}</span>
+         </p>
+         <p class="project__grid">
+           <span class="project__span-one">demo:</span>
+           <span>&lt;<a class="project__span-four" 
+             href="${homepage}" title="${name} - demo">see here</a>&gt;
+           </span>
+         </p>
+         <p class="project__grid">
+           <span class="project__span-one">github:</span>
+           <span>&lt;<a class="project__span-four"
+               href="${html_url}" title="${name} - code">source code</a>&gt;
+           </span>
+         </p>
+       </div>
+     </article>`;
+     if (description)
+     container.innerHTML += template;
+   }
+    
+})
+.catch((e) => console.log(e));
