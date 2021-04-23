1d40b47a36817f6fc22010c09c494044d235e7bb
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2021-04-23 15:17:53 +0200

    style fixes, links in new tab, hand animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,23 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('HELLO 🚀')
+console.log("🌞 Hi! My name is Gosia - nice to meet you 🌞");
 
-fetch ("https://api.github.com/users/malgorzata-ordak/repos")
-.then (resp => resp.json ())
-.then (resp => {
-   const container =document.querySelector('.project-grid--js')
-   for (let repo of resp) {
-       console.log(repo)
-       const {description, homepage, html_url, name}= repo;
-       const template =`<article class="project">
+fetch("https://api.github.com/users/malgorzata-ordak/repos")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    const container = document.querySelector(".project-grid--js");
+    for (let repo of resp) {
+      console.log(repo);
+      const { description, homepage, html_url, name } = repo;
+      const template = `<article class="project">
        <div class="project__bar">
          <span class="project__element"></span>
          <span class="project__element"></span>
          <span class="project__element"></span>
        </div>
        <div class="project__box">
-         <img src="img/Github-Icon.svg" class="project__box-icon" alt="" />
+         <img src="img/Github-Icon.svg"
+          class="project__box-icon" alt="" />
          <h3 class="project__grid project__h3">
            <span class="project__span-one">project: </span>
            <span class="project__span-two">${name}</span>
@@ -27,21 +28,27 @@ fetch ("https://api.github.com/users/malgorzata-ordak/repos")
          </p>
          <p class="project__grid">
            <span class="project__span-one">demo:</span>
-           <span>&lt;<a class="project__span-four" 
-             href="${homepage}" title="${name} - demo">see here</a>&gt;
+           <span>&lt;<a
+           target="_blank"
+           rel="noopener noreferrer" 
+           class="project__span-four" 
+           href="${homepage}"
+           title="${name} - demo">see here</a>&gt;
            </span>
          </p>
          <p class="project__grid">
            <span class="project__span-one">github:</span>
-           <span>&lt;<a class="project__span-four"
-               href="${html_url}" title="${name} - code">source code</a>&gt;
+           <span>&lt;<a
+           target="_blank"
+           rel="noopener noreferrer" 
+           class="project__span-four"
+           href="${html_url}" 
+           title="${name} - code">source code</a>&gt;
            </span>
          </p>
        </div>
      </article>`;
-     if (description)
-     container.innerHTML += template;
-   }
-    
-})
-.catch((e) => console.log(e));
+      if (description) container.innerHTML += template;
+    }
+  })
+  .catch((e) => console.log(e));
